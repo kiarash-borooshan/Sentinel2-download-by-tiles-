a7985083c08065bfd5b1450af930af8c7eee7d5b
kiarash.borooshan <kiarash.borooshan@gmail.com>
2020-11-01 13:54:26 +0330

    make query and DataFram

--- Sentinel report and download/1-S2Down-create_report.py
+++ Sentinel report and download/1-S2Down-create_report.py
@@ -14,8 +14,10 @@ tic_report = time.time()
 a = pd.read_csv("pass.txt")
 user = a.values[0][0]
 password = a.values[1][0]
+del a
 
 api = SentinelAPI(user, password)
+
 
 """ query prepare """
 producttype = "S2MSI1C"
@@ -25,5 +27,82 @@ producttype = "S2MSI1C"
 year_from = "2019"
 year_to = "2019"
 
-""" report """
+""" directories """
 reportName = "ISA - cloud percent less than 35 percent and size.csv"
+# parent_dir = "/media/user/My Passport/Kiarash borooshan"
+parent_dir = "/media/user/406CEBC56CEBB3B6/SSIEC/ISA project/Main Source Codes 2/Sentinel report and download"
+
+""" import tiles name """
+df = pd.read_csv("Sentinel tiles.csv")
+
+""" for """
+products = OrderedDict()
+cloudLessSize = 0
+CloudSizesYear = []
+CloudPrcYear = []
+ProducName = []
+
+n = 1
+num_img = 0
+
+for tile_name in range(df.__len__()):
+    print("**** number: {}". format(n))
+    n += 1
+    print(df.values[tile_name][0])
+
+    for year in range(int(year_from), int(year_to)+1):
+        print(year)
+
+        for Month in range(1, 13):
+            print(Month)
+
+            day_max = "30"
+            if Month in [2]:
+                day_max = "28"
+
+            """ query """
+            query_kwargs = {
+                "platformname": "Sentinel-2",
+                "producttype": producttype,
+                "date": (str(year) + str(Month).zfill(2) + str(1).zfill(2),
+                         str(year) + str(Month).zfill(2) + str(day_max).zfill(2))
+            }
+
+            kw = query_kwargs.copy()
+            kw["tileid"] = df.values[tile_name][0]
+            prePro = api.query(**kw)
+
+            """ detect less cloud tile for each month and more than 400 Mb"""
+
+            mdict = {}
+            for i in prePro:
+                mdict[prePro[i]['cloudcoverpercentage']] = i
+
+            """ report least cloudy image """
+            CldPrcSortTmp = sorted(mdict.keys())
+            for mmin in CldPrcSortTmp:
+                uuid = mdict[mmin]
+                if float(prePro[uuid]["size"][:-3]) > 400 and prePro[uuid]['cloudcoverpercentage'] < 35:
+                    cloudLessSize += float(prePro[uuid]["size"][:-3])
+                    CloudSizesYear.append(float(prePro[uuid]["size"][:-3]))
+                    CloudPrcYear.append(prePro[uuid]['cloudcoverpercentage'])
+                    # break
+
+            CloudSizesYear.append(cloudLessSize)
+            CloudPrcYear.append(str(df.values[tile_name][0]))
+
+""" export for report"""
+data = {
+    "size": CloudSizesYear,
+    "CloudPrcYear": CloudPrcYear
+}
+
+MyDF = pd.DataFrame(data)
+MyDF.insert(2, "status", "not check")
+# print(MyDF)
+
+if os.path.isfile(parent_dir + "/" + "report less cloud percent and size  .csv"):
+    os.remove(parent_dir + "/" + "report less cloud percent and size  .csv")
+    MyDF.to_csv(parent_dir + "/" + "report less cloud percent and size  .csv")
+else:
+    MyDF.to_csv(parent_dir + "/" + "report less cloud percent and size  .csv")
