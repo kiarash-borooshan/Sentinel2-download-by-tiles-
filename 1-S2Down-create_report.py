89353c4673506a9a0c5227cd42d8dec23e6d3d95
kiarash.borooshan <kiarash.borooshan@gmail.com>
2020-11-01 09:14:26 +0330

    API, query, year and tile's name added

--- 1-S2Down-create_report.py
+++ 1-S2Down-create_report.py
@@ -1,3 +1,6 @@
+""" create a report for haw many images are available
+ with specific clouds and date in the tiles"""
+
 from sentinelsat import SentinelAPI
 from collections import OrderedDict
 import pandas as pd
@@ -6,8 +9,21 @@ import os
 
 tic_report = time.time()
 
-""" read password file """
+""" read password file and login """
+
 a = pd.read_csv("pass.txt")
 user = a.values[0][0]
 password = a.values[1][0]
 
+api = SentinelAPI(user, password)
+
+""" query prepare """
+producttype = "S2MSI1C"
+# producttype = "S2MS2Ap"    # doesn't work
+# producttype = "S2MSI2A"    # doesn't work
+
+year_from = "2019"
+year_to = "2019"
+
+""" report """
+reportName = "ISA - cloud percent less than 35 percent and size.csv"
